9ebcb15e23413ac9b891839d457bf858782fb054
Vladimir Badulenko <vkb030@gmail.com>
2023-04-12 14:17:34 +0700

    Добавлен code для решения задачи

--- TestCode/Program.cs
+++ TestCode/Program.cs
@@ -1,35 +1,35 @@
 ﻿// Первый способ
 
-// Console.Clear();
+Console.Clear();
 
-// string[] arr = new string[6] {"124", "23", "hello", "world", "rus", "test"};
-// string[] newArr = new string[arr.Length];
+string[] arr = new string[6] {"124", "23", "hello", "world", "rus", "test"};
+string[] newArr = new string[arr.Length];
 
-// void ArrayCreationByTask(string[] arr, string[] newArr)
-// {
-//     int j = 0;
-//     for (int i = 0; i < arr.Length; i++)
-//     {
-//     if(arr[i].Length <= 3)
-//         {
-//         newArr[j] = arr[i];
-//         j++;
-//         }
-//     }
-// }
+void ArrayCreationByTask(string[] arr, string[] newArr)
+{
+    int j = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+    if(arr[i].Length <= 3)
+        {
+        newArr[j] = arr[i];
+        j++;
+        }
+    }
+}
 
-// void ArrayPrinted(string[] array)
-// {
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         Console.Write($"{array[i]} ");
-//     }
-//     Console.WriteLine();
-// }
+void ArrayPrinted(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
 
-// ArrayCreationByTask(arr, newArr);
-// ArrayPrinted(newArr);
-// Console.WriteLine();
+ArrayCreationByTask(arr, newArr);
+ArrayPrinted(newArr);
+Console.WriteLine();
 
 // Второй способ
 
